5afc9338918135c58083b4eb2b5d9b9e8068afcd
Lorenna Silva <devlorenna@gmail.com>
2024-06-13 19:22:33 -0300

    Atualizei teste.app
    Coloquei um código teste que eu fiz no TinkerCad. A parte principal desse código é o uso da função millis(), que é uma alternativa ao método delay(), em que o programa não precisa ser interrompido para que haja verificação de algo. Eu não sei como usar o sensor ultrassônico no site, depois é necessário testar com sensores reais.

--- scripts/teste.cpp
+++ scripts/teste.cpp
@@ -1,7 +1,19 @@
-void start(){
-    
+const int pinVibracao = 7;
+unsigned long tempoAnterior = 0;
+const long intervalo = 500;
+
+void setup()
+{
+  pinMode(pinVibracao, OUTPUT);
 }
 
-void loop(){
-
+void loop()
+{
+  unsigned long tempoAtual = millis();
+  
+  if(tempoAtual - tempoAnterior >= intervalo){
+    tempoAnterior = tempoAtual;
+    int estadoVibracao = digitalRead(pinVibracao);
+    digitalWrite(pinVibracao, !estadoVibracao);
+  }
 }
