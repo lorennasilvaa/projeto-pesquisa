f150005176ad68ba782341de9705335db56e2c27
Lorenna Silva <161973520+lorennasilvaa@users.noreply.github.com>
2024-06-14 08:33:31 -0300

    Update teste01.cpp

--- scripts/teste01.cpp
+++ scripts/teste01.cpp
@@ -1,11 +1,11 @@
 const int trigPin = 9;
 const int echoPin = 10;
 const int ledPin = 13;
-const long distanceThreshold = 10; // Distância em cm
+const long distanciaLimite = 10; // Distância em cm
 
-unsigned long previousMillis = 0;  // Armazena o último tempo em que o LED piscou
-const long interval = 500;         // Intervalo de tempo em milissegundos (0.5 segundos)
-bool ledState = LOW;               // Estado atual do LED
+unsigned long ultimoTempo = 0;  // Armazena o último tempo em que o LED piscou
+const long intervalo = 10;         // Intervalo de tempo em milissegundos (0.5 segundos)
+bool estadoLed = LOW;               // Estado atual do LED
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -15,8 +15,8 @@ void setup() {
 }
 
 void loop() {
-  long duration, distance;
-  unsigned long currentMillis = millis();
+  long duracao, distancia;
+  unsigned long tempoAtual = millis();
 
   // Envia um pulso de 10us para o pino Trig
   digitalWrite(trigPin, LOW);
@@ -26,32 +26,32 @@ void loop() {
   digitalWrite(trigPin, LOW);
 
   // Lê o tempo de retorno do pulso no pino Echo
-  duration = pulseIn(echoPin, HIGH);
+  duracao = pulseIn(echoPin, HIGH);
 
   // Calcula a distância (duração / 2 porque o pulso vai e volta,
   // e / 29.1 para converter de microsegundos para cm)
-  distance = duration / 29.1 / 2;
+  distancia = duracao / 29.1 / 2;
 
   // Imprime a distância no monitor serial
   Serial.print("Distance: ");
-  Serial.print(distance);
+  Serial.print(distancia);
   Serial.println(" cm");
 
   // Verifica se a distância é menor que 10 cm
-  if (distance < distanceThreshold) {
+  if (distancia < distanciaLimite) {
     // Verifica se o intervalo passou
-    if (currentMillis - previousMillis >= interval) {
+    if (tempoAtual - ultimoTempo >= intervalo) {
       // Salva o tempo da última mudança
-      previousMillis = currentMillis;
+      ultimoTempo = tempoAtual;
 
       // Inverte o estado do LED
-      ledState = !ledState;
-      digitalWrite(ledPin, ledState);
+      estadoLed = !estadoLed;
+      digitalWrite(ledPin, estadoLed);
     }
   } else {
     // Garante que o LED esteja desligado se a distância for maior ou igual a 10 cm
     digitalWrite(ledPin, LOW);
-    ledState = LOW;
+    estadoLed = LOW;
   }
 
   delay(100); // Pequeno atraso para estabilidade
